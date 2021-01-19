ee1c2a48a0fb64e1d9e9f9237cf9af74402ef7cd
rudwn9641 <rudwn9641@naver.com>
2021-01-19 17:58:09 +0900

    3-4 내용 추가

--- data.php
+++ data.php
@@ -4,25 +4,71 @@ $siteTitle = "디자이너 구미베어";
 // 게시물 4
 $article4 = [];
 $article4["id"] = 4;
-$article4["title"] = "4화, 자바스크립트 태그 4";
+$article4["title"] = "4화, align-items";
 $article4["regDate"] = "2020-01-18 17:28:15";
-$article4["writerName"] = "홍길동";
-$article4["writerAvatar"] = '<svg viewBox="0 0 264 280"><use xlink:href="#avatar-1"></use></svg>';
+$article2["writerName"] = "ʕ•ᴥ•ʔ";
+$article2["writerAvatar"] = "임경주";
 $article4["body"] = <<<EOT
-# 개요
-- 안녕하세요.
+- flex-start: 요소들을 컨테이너의 꼭대기로 정렬합니다.
+- flex-end: 요소들을 컨테이너의 바닥으로 정렬합니다.
+- center: 요소들을 컨테이너의 세로선 상의 가운데로 정렬합니다.
+- baseline: 요소들을 컨테이너의 시작 위치에 정렬합니다.
+- stretch: 요소들을 컨테이너에 맞도록 늘립니다..
+
+# 실습
+```codepen
+https://codepen.io/rudwn9641/embed/jOMdgxd?height=265&theme-id=light&default-tab=css,result
+```
 EOT;
 
 // 게시물 3
 $article3 = [];
 $article3["id"] = 3;
-$article3["title"] = "3화, 자바스크립트 태그 3";
+$article3["title"] = "3화, justifiy-content";
 $article3["regDate"] = "2020-01-12 12:12:15";
-$article3["writerName"] = "홍길동";
-$article3["writerAvatar"] = '<svg viewBox="0 0 264 280"><use xlink:href="#avatar-1"></use></svg>';
+$article2["writerName"] = "ʕ•ᴥ•ʔ";
+$article2["writerAvatar"] = "임경주";
 $article3["body"] = <<<EOT
 # 개요
-- php도 좋다.
+- 콘텐츠를 수평(행) 정렬하는 속성이다.
+- 콘텐츠(item)의 가로 넓이가 컨테이너보다 작아야 한다.
+- flex-start: 요소들을 컨테이너의 왼쪽으로 정렬합니다.
+- flex-end: 요소들을 컨테이너의 오른쪽으로 정렬합니다.
+- center: 요소들을 컨테이너의 가운데로 정렬합니다.
+- space-between: 요소들 사이에 동일한 간격을 둡니다.
+- space-around: 요소들 주위에 동일한 간격을 둡니다.
+
+# 예시
+```html
+<!doctype>
+<html>
+    <head>
+        <style>
+            .flex-container {
+                display: flex;
+                justify-content: space-between; /* 진행방향 가로!! flex-start, center, flex-end, space-around, space-evenly, space-between */
+            }
+
+            .item {
+                border: 5px solid black;
+                width: 200px;
+                height: 100px;
+
+                display: flex;
+                justify-content: flex-end; /* 진행 방향(main axis)의 정렬 */
+                align-items: flex-end; /* 진행 방향의 반대(세로)(cross axis) 정렬 */
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-container">
+            <div class="item">첫번째 박스</div>
+            <div class="item">두번째 박스</div>
+            <div class="item">세번째 박스</div>
+        </div>
+    </body>
+</html>
+```
 EOT;
 
 // 게시물 2
