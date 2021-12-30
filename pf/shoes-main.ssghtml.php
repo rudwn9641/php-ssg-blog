45510686b8e39604274a2c27b4a86be84c045ee7
rudwn9641 <rudwn9641@naver.com>
2021-12-30 22:31:20 +0900

    html 다시 추출ㅠ

--- pf/shoes-main.ssghtml.php
+++ pf/shoes-main.ssghtml.php
@@ -8,7 +8,7 @@ require_once "shoes-head.htmlssg.php";
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>임경주 포트폴리오</title>
   <link rel="stylesheet" href="shoes-main.css">
   <script src="shoes-main.js" defer></script>
 </head>
