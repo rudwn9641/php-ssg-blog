5184960ae45354f2133057cac77a8d4d6c0d6a5d
rudwn9641 <rudwn9641@naver.com>
2022-01-01 23:10:13 +0900

    슬라이드

--- pf/shoes-pf.ssghtml.php
+++ pf/shoes-pf.ssghtml.php
@@ -11,31 +11,35 @@ require_once "shoes-head.ssghtml.php";
     <h1>Portfolio</h1>
     <div class="page page-1" id="page-1">
         <div class="section section-1">
-            <div class="my-slider-box-1__nav">
-                <!-- Add Arrows -->
-                <div class="my-slider-box-1__btn-left"><i class="fas fa-caret-left"></i></div>
-                <div class="my-slider-box-1__btn-right"><i class="fas fa-caret-right"></i></div>
-            </div>
-            <div class="mockup mockup-1">
-                <div>
-                    <img src="https://i.postimg.cc/Dw9QJFvR/i-Mac-27-Mockup.jpg" alt="">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="my-slider-box-1__nav">
+                        <!-- Add Arrows -->
+                        <div class="my-slider-box-1__btn-left"><i class="fas fa-caret-left"></i></div>
+                        <div class="my-slider-box-1__btn-right"><i class="fas fa-caret-right"></i></div>
+                    </div>
+                    <div class="mockup mockup-1">
+                        <div>
+                            <img src="https://i.postimg.cc/Dw9QJFvR/i-Mac-27-Mockup.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="title title-1">INTERNI</div>
+                    <div class="title title-2">& Decor</div>
+                    <span class="view view-box-1">
+                        <span>
+                            <span><a href="#">Process View</a></span>
+                            <span><a href="#">Process View</a></span>
+                        </span>
+                        <div class="line"></div>
+                    </span>
+                    <span class="view view-box-2">
+                        <span>
+                            <span><a href="#">Design View</a></span>
+                            <span><a href="#">Design View</a></span>
+                        </span>
+                    </span>
                 </div>
             </div>
-            <div class="title title-1">INTERNI</div>
-            <div class="title title-2">& Decor</div>
-            <span class="view view-box-1">
-                <span>
-                    <span><a href="#">Process View</a></span>
-                    <span><a href="#">Process View</a></span>
-                </span>
-                <div class="line"></div>
-            </span>
-            <span class="view view-box-2">
-                <span>
-                    <span><a href="#">Design View</a></span>
-                    <span><a href="#">Design View</a></span>
-                </span>
-            </span>
         </div>
     </div>
     <div class="page page-2" id="page-2">
